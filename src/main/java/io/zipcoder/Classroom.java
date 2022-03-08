2d234cff9a4f92179d6aa87bfe25c8b8cb6fcae8
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-08 17:06:53 -0500

    classroom started

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,37 @@
 package io.zipcoder;
 
 public class Classroom {
+
+    public Student[] students;
+
+    public Classroom(int maxNumberOfStudents){
+
+        students = new Student[maxNumberOfStudents];
+    }
+
+    public Classroom(Student[] students){
+
+        this.students = students;
+    }
+
+    public Classroom(){
+
+        this.students = new Student[30];
+    }
+
+    public Student[] getStudents(){
+
+        return students;
+    }
+
+    public Double getAverageExamScore(){
+
+        Double average = 0.0;
+        for( int i = 0; i <this.students.length; i++){
+            average += students[i].getAverageExamScore();
+        }
+        return average / this.students.length;
+    }
+
+
 }
