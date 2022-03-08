6e9366f532f4815c5382516a7741f251081a7d03
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-08 17:27:08 -0500

    remove students

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -6,49 +6,60 @@ public class Classroom {
 
     public Student[] students;
 
-    public Classroom(int maxNumberOfStudents){
+    public Classroom(int maxNumberOfStudents) {
 
         students = new Student[maxNumberOfStudents];
     }
 
-    public Classroom(Student[] students){
+    public Classroom(Student[] students) {
 
         this.students = students;
     }
 
-    public Classroom(){
+    public Classroom() {
 
         this.students = new Student[30];
     }
 
-    public Student[] getStudents(){
+    public Student[] getStudents() {
 
         return students;
     }
 
-    public Double getAverageExamScore(){
+    public Double getAverageExamScore() {
 
         Double average = 0.0;
-        for( int i = 0; i <this.students.length; i++){
+        for (int i = 0; i < this.students.length; i++) {
             average += students[i].getAverageExamScore();
         }
         return average / this.students.length;
     }
 
-    public void addStudent (Student student){
+    public void addStudent(Student student) {
 
         ArrayList<Student> newArray = new ArrayList<>();
         newArray.add(student);
 
-        for (int i = 0; i <this.students.length; i++){
-            if (students[i] == null){
+        for (int i = 0; i < this.students.length; i++) {
+            if (students[i] == null) {
                 students[i] = newArray.get(0);
                 break;
             }
         }
     }
 
-    public void removeStudent (String firstName, String lastName){
+    public void removeStudent(String firstName, String lastName) {
 
+        for (int i = 0; i < this.students.length; i++){
+            if (students[i].getFirstName().equals(firstName) && students[i].getLastName().equals(lastName)) {
+                students[i] = null;
+            }
+        }
+        for (int i = 0; i < this.students.length -1; i++){
+            if (students[i] == null){
+                students[i] = students[i+1];
+                students[i+1] = null;
+            }
+        }
     }
 }
