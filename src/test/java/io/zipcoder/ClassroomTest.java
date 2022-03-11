cae63ba734c88be481c331304d7e55d977af089b
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-10 20:50:22 -0500

    test started

--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,18 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ClassroomTest {
+
+    @Test
+    public void testClassroom(){
+        //Given
+        Student[] expected = new Student[30];
+        //When
+        Classroom classroom = new Classroom();
+        Student[] actual = classroom.getStudents();
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
 }
